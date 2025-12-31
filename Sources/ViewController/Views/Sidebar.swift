231f1b8de608206d44afc6fe13ea6fe21fe0b38e
Michael Housh <michael@mhoush.com>
2025-12-30 20:22:53 -0500

    WIP: Adds initial sidebar, needs more styling.

--- Sources/ViewController/Views/Sidebar.swift
+++ Sources/ViewController/Views/Sidebar.swift
@@ -6,7 +6,7 @@ struct Sidebar: HTML {
     div(
       .class(
         """
-          h-screen w-64 border-r-3 bg-gray-100 shadow space-y-4
+          h-screen w-64 pt-10 border-r-3 border-gray-800 bg-gray-100 shadow
         """)
     ) {
       row(title: "Project", icon: "map-pin")
@@ -17,20 +17,20 @@ struct Sidebar: HTML {
     }
   }
 
-  func row(
+  private func row(
     title: String,
     icon: String
   ) -> some HTML {
     button(
       .class(
         """
-        flex w-full jusitfy-between items-center text-gray-800 hover:bg-gray-300
+        flex w-full items-center gap-4 text-gray-800 hover:bg-gray-300 pl-4 py-2
         """
       )
     ) {
       i(.data("lucide", value: icon)) {}
       p(
-        .class("text-2xl flex-1")
+        .class("text-xl font-bold")
       ) {
         title
       }
